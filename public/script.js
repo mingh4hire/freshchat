67925e6a8a931c5ff9a1b39843e756f3cd97905b
m <mingh4hire@gmail.com>
2025-01-13 02:44:04 -0600

    socket integration

--- public/script.js
+++ public/script.js
@@ -3,11 +3,14 @@
  
  var userId='';
 
- const videoGrid = document.getElementById('video-grid')
+
+
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
   port: 443,
- path: '/peerjs/myapp2'
+  path: '/peerjs/myapp2'
   //
   // secure: true
 })
@@ -21,39 +24,88 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
   addVideoStream(myVideo, stream)
 
+  socket.on('user-connected', (peerId) => {
+    console.log('someone else has been connected', peerId);
+    const userElement = document.createElement('div')
+    userElement.innerText = `New user connected : ${peerId}`
+    
+    const userListElement = document.getElementById('userList')
+    userListElement.appendChild(userElement)
+    console.log('about to connect to user on stream' ,peerId, stream);
+    setTimeout(() => {
+      connectToNewUser(peerId, stream);
+    }, 1000);
+    // connectToNewUser(peerId, stream);
+  
+  });
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
+    call.on("close", function() {
+      console.log("closingit", call.peer);
+      video.remove();
+      fetch('/remove?id='+call.peer)
+    });
+
   })
   outgoingstream = stream;
 })
  
 myPeer.on('open', id => {
   userId = id;
-  console.log(userId);
-  // fetch('/add?id='+id)
-  // .then(response => response.json())
-  // .then(data => {
-  //     console.log('response is', data);
-  // })
-  // .catch(error => {
-  //     console.error('Error fetching /a:', error);
-  // });
+  console.log('joining room ' , ROOM_ID, id);
+  socket.emit('join-room', ROOM_ID, id);
+
+  document.querySelector('#myId').innerHTML = id;
+  fetch('/add?id='+id)
+  .then(response => response.json())
+  .then(data => {
+      console.log('response is', data);
+  })
+  .catch(error => {
+      console.error('Error fetching /a:', error);
+  });
+});
+
+myPeer.on('disconnect', id => {
+  alert('disconnected');
+  fetch('/remove?id='+id)
+  .then(response => response.json())
+  .then(data => {
+      console.log('response is', data);
+  })
+  .catch(error => {
+      console.error('Error fetching /a:', error);
+  });
+});
+myPeer.on('close', id => {
+  alert('closed');
+  fetch('/remove?id='+id)
+  .then(response => response.json())
+  .then(data => {
+      console.log('response is', data);
+  })
+  .catch(error => {
+      console.error('Error fetching /a:', error);
+  });
 });
 
 function connectToNewUser(userId, stream) {
+  console.log(userId, 'userid', stream, 'stream');
   const call = myPeer.call(userId, stream)
+  console.log('calling', call);
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
   call.on('close', () => {
     video.remove()
-  })
-
+    console.log("closingit2", call.peer);
+    fetch('/remove?id='+call.peer)
+  })    
   peers[userId] = call
 }
 
@@ -62,7 +114,34 @@ function addVideoStream(video, stream) {
   video.addEventListener('loadedmetadata', () => {
     video.play()
   });
-
-  
   videoGrid.append(video)
 }
+ 
+function sendMessage() {
+  const messageInput = document.querySelector('#messageInput');
+  const message = messageInput.value;
+  socket.emit('message', message);
+  messageInput.value = '';
+}
+const messageList = document.querySelector('#messageList');
+
+  // Function to add a new message to the list
+  function addMessage(message) {
+    const newMessage = document.createElement('div');
+    newMessage.textContent = message;
+    messageList.appendChild(newMessage);
+  }
+
+  // Socket event listener for receiving messages
+  socket.on('message', (message) => {
+    console.log(message);
+    addMessage(message);
+  });
+
+
+  // const myPeer = new Peer(undefined, { path: '/peerjs', host: '/', port: 9000 });
+
+  // setupSocketConnectionViaPeer(socket, myPeer);
+  // setupSocketEvents(socket);
+
+
--- server.js
+++ server.js
@@ -10,18 +10,59 @@ const options = {
   honorCipherOrder: true,
   secureProtocol: 'TLSv1_2_method'
 };
+const httpApp = express();
+
+httpApp.get('*', (req, res) => {
+  res.redirect('https://' + req.headers.host + req.url);
+});
+
+http= require('http')
+const httpServer = http.createServer(httpApp);
+httpServer.listen(80);
+
 const https = require('https');
 const server = https.createServer(options, app);
 const list = [];
+const sockets = require('socket.io');
+
+const socketServer = sockets(server, {
+  // path: 
+  path: '/socket.io'
+});
 
 // const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 // const { PeerServer } = require("peer");
 // const peerServer = PeerServer({ port: 3000, path: "/" });
 var ExpressPeerServer = require('peer').ExpressPeerServer;
+const session = require('express-session');
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false, 
+  saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.post('/', (req, res) => {
+  const { username, password } = req.body;
+  if (password === 'excite') {
+    req.session.username = username;
+    res.redirect('/');
+  } else {
+    res.sendFile(__dirname + '/public/login.html');
+  }
+});
 
-// const peerServer = PeerServer({  port:3002, path: "/peer" });//, secureProtocol: 'TLSv1_2_method'});
-app.set('view engine', 'ejs')
+app.use((req, res, next) => {
+  console.log('session', req.session);
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.sendFile(__dirname + '/public/login.html');
+  }
+});
+
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,8 +75,12 @@ app.get('/list', (req, res) => {
 }) ;
 app.get('/add', (req, res) => {
   list.push(req.query.id);
-  res.send();
-}) ;
+  res.json({});
+});
+app.get('/remove', (req, res) => {
+  list.splice(list.indexOf(req.query.id), 1);
+  res.json();
+});
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
@@ -43,20 +88,29 @@ const peerServer = ExpressPeerServer(server, {
    path: '/myapp2' // Optional: specify a path for the PeerJS server
 });
 
-console.log(peerServer);
+// console.log(peerServer);
 app.use('/peerjs', peerServer); // Mount the 
-//  server at a specific path
 
-// io.on('connection', socket => {
-//   socket.on('join-room', (roomId, userId) => {
-//     socket.join(roomId)
-//     socket.to(roomId).broadcast.emit('user-connected', userId)
-//     list.push(userId);
-//     socket.on('disconnect', () => {
-//       socket.to(roomId).broadcast.emit('user-disconnected', userId);
-//       list.splice(list.indexOf(userId), 1);
-//     })
-//   })
-// })
 
 server.listen(443)
+socketServer.on("connection", (socket) => {
+  console.log('socket connected');
+  socket.on('join-room', (roomId, peerId) => {
+    console.log('someone joined the room ', roomId, peerId);
+    
+    // socket.join(roomId);
+    // socket.join(roomId);
+    // socket.broadcast.to(roomId).emit('message', 'Someone else joined');
+    // sockets.sockets.emit('user-connected', peerId, 'joined the room');
+    socket.broadcast.emit('user-connected', peerId, 'joined the room');
+    // socket.broadcast.emit('message', peerId, 'joined the room');
+  });
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+  socket.on('message', (message) => {
+    try{
+      socket.broadcast.emit('message',message);
+    }catch(e){}
+  });
+});
